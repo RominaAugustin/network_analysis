c4280ea0e75abd556465a5a6a5f801731f353a1e
RominaAugustin <60093420+RominaAugustin@users.noreply.github.com>
2020-01-20 17:02:06 +0100

    Added package installing script

---
+++ 00 installing packages_download data 20200119.R
@@ -0,0 +1,49 @@
+####################################################################################################################################
+#         Seminar Network Analysis 20.01-24.01.2020
+####################################################################################################################################
+
+# Preparations d -1 19.01.2020
+# Installing packages and downloading data for practicals
+
+# set wd (use double backslash to have a real backslash like in directory)
+setwd("D:\\Master\\WS1920 Seminar Network Analysis")
+
+# download datasets from dropbox
+
+## install packages
+install.packages('devtools') 
+install.packages('tidyr')
+install.packages('dplyr')
+install.packages('purrr')
+install.packages('curl')
+install.packages('ggplot2')
+# From Bioconductor
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("viper")
+library(viper)
+
+BiocManager::install("progeny")
+library(progeny)
+
+BiocManager::install("fgsea")
+library(fgsea)
+
+BiocManager::install("OmnipathR")
+library(OmnipathR)
+
+# From GitHub
+devtools::install_github('saezlab/CellNOptR')
+devtools::install_github('saezlab/CNORode')
+
+## load packages
+library(usethis) #needed to load devtools
+library(devtools)
+library(tidyr)
+library(dplyr)
+library(purrr)
+library(curl)
+library(ggplot2)
+
+## install CARNIVAL
